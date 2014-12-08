d4825bb3d442f3247bd9863b382f79869bc85f8a
busungg <busungg@naver.com>
2014-12-08 23:38:13 +0900

    APP, API Web Commit
    1. App 0.6 ver
    - playlist detail activity finish
    
    2. API Web 0.2 ver
    - playlist detail API finish

---
+++ API Web/0.2 ver/MvcApplication1/MvcApplication1/Controllers/MusiclistController.cs
@@ -0,0 +1,449 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services;
+using System.Web.Script.Serialization;
+using System.Web.Mvc;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Data;
+using System.Collections;
+
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+namespace MvcApplication1.Controllers
+{
+    
+    public class MusiclistController : Controller
+    {
+
+        public const string SOUNDCLOUD_CLIENT_KEY = "a2b4d87e3bac428d8467d6ea343d49ae";
+        public const string FAIL_STRING = "{ \"result\":fail}";
+
+        public const string RESULT_FIRST_STRING = "{ \"list\":";
+        public const string RESULT_LAST_STRING = "}";
+
+        public ActionResult MusicList()
+        {
+            return View();
+        }
+
+        public string YoutubeURLValidation(string VID)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://gdata.youtube.com/feeds/api/videos/" + VID);
+                request.Method = "HEAD";
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {
+                        return FAIL_STRING;
+                    }
+                    else
+                    {
+                        var xmlDoc = new XmlDocument();
+                        xmlDoc.Load("http://gdata.youtube.com/feeds/api/videos/" + VID);
+                        XmlNodeList title = xmlDoc.GetElementsByTagName("title");
+                        return (title.Item(0)).InnerText;
+                    }
+                }
+            }
+            catch(Exception ex){
+                return FAIL_STRING;
+            }
+        }
+
+        public class SoundcloudValidationData
+        {
+            public string title { get; set; }
+            public string uri { get; set; }
+        }
+
+        public string SoundcloudURLValidation(string URL)
+        {
+            string url = "http://api.soundcloud.com/resolve.json?url=" + URL + "&client_id=" + SOUNDCLOUD_CLIENT_KEY;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+               using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+               {
+
+                    if (response.StatusCode != HttpStatusCode.OK)
+                    {
+                        return FAIL_STRING;
+                    }
+                    else
+                    {
+                        Stream stream = response.GetResponseStream();
+                        StreamReader reader = new StreamReader(stream);
+                        string result = reader.ReadToEnd();
+                        stream.Close();
+                        response.Close();
+                        JObject parsedResult = JObject.Parse(result);
+
+                        SoundcloudValidationData SVD = new SoundcloudValidationData
+                        {
+                            title = parsedResult["title"].ToString(),
+                            uri = parsedResult["uri"].ToString()
+                        };
+
+                        return JsonConvert.SerializeObject(SVD, Newtonsoft.Json.Formatting.Indented).ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return FAIL_STRING;
+            }
+        }
+
+        public int maxResults = 10;
+
+        public class SearchDataModel
+        {
+            public string id { get; set; } //Soundcould : stream_url, Youtube : id
+            public string title { get; set; }
+            public string duration { get; set; }
+            public string thumbnail { get; set; } //Soundcloud : artwork_url
+            public string viewCount { get; set; } //Soundcould : playback_count
+            //public int stream_url { get; set; }
+        }
+
+        public string YoutubeTrackSearch(string keyword, int index)
+        {
+            string url = "http://gdata.youtube.com/feeds/api/videos?q=" + keyword + "&max-results=" + maxResults + "&alt=jsonc&v=2&start-index=" + index;
+            
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                string result = reader.ReadToEnd();
+                stream.Close();
+                response.Close();
+                
+                JObject parsedResult = JObject.Parse(result);
+                JArray items = (JArray)parsedResult["data"]["items"];
+                ArrayList dataList = new ArrayList();
+
+                foreach (JObject resultOne in items)
+                {                    
+                    try
+                    {
+                        SearchDataModel searchDataModel = new SearchDataModel
+                        {
+                            id = resultOne["id"].ToString(),
+                            title = resultOne["title"].ToString(),
+                            thumbnail = resultOne["thumbnail"]["sqDefault"].ToString(),
+                            viewCount = resultOne["viewCount"].ToString(),
+                            duration = resultOne["duration"].ToString()
+                        };
+
+                        dataList.Add(searchDataModel);
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                }
+
+                return RESULT_FIRST_STRING + JsonConvert.SerializeObject(dataList) + RESULT_LAST_STRING;
+            }
+            catch (Exception ex)
+            {
+                return FAIL_STRING;
+            }
+        }
+
+        public string SoundcloudTrackSearch(string keyword, int index)
+        {
+            string url = "https://api.soundcloud.com/tracks.json?client_id=" + SOUNDCLOUD_CLIENT_KEY + "&q= " + keyword + "&&limit="+ maxResults +"&offset=" + index;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                string result = reader.ReadToEnd();
+                stream.Close();
+                response.Close();
+
+                JArray items = JArray.Parse(result);
+                ArrayList dataList = new ArrayList();
+
+                foreach (JObject resultOne in items)
+                {
+                    try
+                    {
+                        SearchDataModel searchDataModel = new SearchDataModel
+                        {
+                            id = resultOne["id"].ToString(),
+                            title = resultOne["title"].ToString(),
+                            thumbnail = (resultOne["artwork_url"] == null ? "" : resultOne["artwork_url"]).ToString(),
+                            viewCount = resultOne["playback_count"].ToString(),
+                            duration = resultOne["duration"].ToString()
+                        };
+
+                        dataList.Add(searchDataModel);
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                }
+
+                return RESULT_FIRST_STRING + JsonConvert.SerializeObject(dataList) + RESULT_LAST_STRING;
+            }
+            catch (Exception ex)
+            {
+                return FAIL_STRING;
+            }
+        }
+
+        public int YOUTUBE_TYPE = 1;
+        public int SOUND_CLOUD_TYPE = 2;
+
+        public class SearchPlaylistModel
+        {
+            public string id { get; set; } //Soundcould : id, Youtube : id
+            public string title { get; set; }
+            public string thumbnail { get; set; } //Soundcloud : artwork_url
+            public string size { get; set; } //Soundcould : track_count
+            public int type { get; set; } // Youtube, Soundcloud 1,2
+        }
+
+
+        public string YoutubePlaylistSearch(string keyword, int index)
+        {
+            string url = "https://gdata.youtube.com/feeds/api/playlists/snippets?&q=" + keyword + "&max-results=" + maxResults + "&alt=jsonc&v=2&start-index=" + index;
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                string result = reader.ReadToEnd();
+                stream.Close();
+                response.Close();
+
+                JObject parsedResult = JObject.Parse(result);
+                JArray items = (JArray)parsedResult["data"]["items"];
+                ArrayList dataList = new ArrayList();
+
+                foreach (JObject resultOne in items)
+                {
+                    try
+                    {
+                        SearchPlaylistModel searchPlaylistModel = new SearchPlaylistModel
+                        {
+                            id = resultOne["id"].ToString(),
+                            title = resultOne["title"].ToString(),
+                            thumbnail = resultOne["thumbnail"]["sqDefault"].ToString(),
+                            size = resultOne["size"].ToString(),
+                            type = YOUTUBE_TYPE
+                        };
+
+                        dataList.Add(searchPlaylistModel);
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                }
+
+                return RESULT_FIRST_STRING + JsonConvert.SerializeObject(dataList) + RESULT_LAST_STRING;
+            }
+            catch (Exception ex)
+            {
+                return FAIL_STRING;
+            }
+        }
+
+        public string SoundcloudPlaylistSearch(string keyword, int index)
+        {
+            string url = "https://api.soundcloud.com/playlists.json?client_id=" + SOUNDCLOUD_CLIENT_KEY + "&q= " + keyword + "&limit=" + maxResults + "&offset=" + index;
+            
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                string result = reader.ReadToEnd();
+                stream.Close();
+                response.Close();
+
+                JArray items = JArray.Parse(result);
+                ArrayList dataList = new ArrayList();
+
+                foreach (JObject resultOne in items)
+                {
+                    try
+                    {
+                        SearchPlaylistModel searchPlaylistModel = new SearchPlaylistModel
+                        {
+                            id = resultOne["id"].ToString(),
+                            title = resultOne["title"].ToString(),
+                            thumbnail = resultOne["tracks"][0]["artwork_url"].ToString(),
+                            size = resultOne["track_count"].ToString(),
+                            type = SOUND_CLOUD_TYPE
+                        };
+
+                        dataList.Add(searchPlaylistModel);
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                }
+
+                return RESULT_FIRST_STRING + JsonConvert.SerializeObject(dataList) + RESULT_LAST_STRING;
+            }
+            catch (Exception ex)
+            {
+                return FAIL_STRING;
+            }
+        }
+
+        //API는 분리한다.
+        public string YoutubePlaylistDetailSearch(string id, int index)
+        {
+            string url = "https://gdata.youtube.com/feeds/api/playlists/" + id + "?start-index="+ index +"&max-results=" + maxResults + "&alt=jsonc&v=2";
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                string result = reader.ReadToEnd();
+                stream.Close();
+                response.Close();
+
+                JObject parsedResult = JObject.Parse(result);
+                JArray items = (JArray)parsedResult["data"]["items"];
+                ArrayList dataList = new ArrayList();
+
+                foreach (JObject resultOne in items)
+                {
+                    try
+                    {
+                        SearchDataModel searchDataModel = new SearchDataModel
+                        {
+                            id = resultOne["video"]["id"].ToString(),
+                            title = resultOne["video"]["title"].ToString(),
+                            thumbnail = resultOne["video"]["thumbnail"]["sqDefault"].ToString(),
+                            viewCount = resultOne["video"]["viewCount"].ToString(),
+                            duration = resultOne["video"]["duration"].ToString()
+                        };
+
+                        dataList.Add(searchDataModel);
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                }
+
+                return RESULT_FIRST_STRING + JsonConvert.SerializeObject(dataList) + RESULT_LAST_STRING;
+            }
+            catch (Exception ex)
+            {
+                return FAIL_STRING;
+            }
+        }
+
+        public string SoundCloudPlaylistDetailSearch(string id, int index)
+        {
+            string url = "https://api.soundcloud.com/playlists/"+ id + ".json?client_id=" + SOUNDCLOUD_CLIENT_KEY + "&limit=" + maxResults + "&offset=" + index;
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                string result = reader.ReadToEnd();
+                stream.Close();
+                response.Close();
+
+                JObject parsedResult = JObject.Parse(result);
+                JArray items = (JArray)parsedResult["tracks"];
+                ArrayList dataList = new ArrayList();
+
+                foreach (JObject resultOne in items)
+                {
+                    try
+                    {
+                        SearchDataModel searchDataModel = new SearchDataModel
+                        {
+                            id = resultOne["id"].ToString(),
+                            title = resultOne["title"].ToString(),
+                            thumbnail = (resultOne["artwork_url"] == null ? "" : resultOne["artwork_url"]).ToString(),
+                            viewCount = resultOne["playback_count"].ToString(),
+                            duration = resultOne["duration"].ToString()
+                        };
+
+                        dataList.Add(searchDataModel);
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                }
+
+                return RESULT_FIRST_STRING + JsonConvert.SerializeObject(dataList) + RESULT_LAST_STRING;
+            }
+            catch (Exception ex)
+            {
+                return FAIL_STRING;
+            }
+        }
+
+        public string YoutubeKeywordV3Search(string keyword, string index)
+        {
+            string apiKey = "AIzaSyC_2UXHjm8y2QceUdJFGyKCsv-BdyOoABg";
+
+            YouTubeService youtube = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = apiKey
+            });
+
+            SearchResource.ListRequest listRequest = youtube.Search.List("snippet");
+            listRequest.Q = keyword;
+            listRequest.MaxResults = maxResults;
+            listRequest.Type = "video";
+
+            if (index != null)
+            {
+                listRequest.PageToken = index;
+            }
+
+            SearchListResponse searchResponse = listRequest.Execute();
+
+            ArrayList dataList = new ArrayList();
+
+            foreach (SearchResult searchResult in searchResponse.Items)
+            {
+                SearchDataModel SDM = new SearchDataModel
+                {
+                    id = searchResult.Id.VideoId,
+                    title = searchResult.Snippet.Title,
+                    thumbnail = searchResult.Snippet.Thumbnails.Default.Url,
+                    //viewCount = searchResult.Snippet,
+                    //duration = resultOne["duration"].ToString()
+                };
+
+                dataList.Add(SDM);
+            }
+
+            return "null";
+        }
+    }
+}
---
+++ Mobile/APP/MusicStyleList 0.6ver/MusicStyleList/src/com/funny/developers/musicstylelist/fragment/FragmentForPlayList.java
@@ -0,0 +1,117 @@
+package com.funny.developers.musicstylelist.fragment;
+
+import org.json.JSONObject;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+import com.funny.developers.baseconnectionlib.consts.BaseConsts;
+import com.funny.developers.musicstylelist.activity.PlayListDetailActivity;
+import com.funny.developers.musicstylelist.adapter.PlayListAdapter;
+import com.funny.developers.musicstylelist.baseactivity.R;
+import com.funny.developers.musicstylelist.basefragment.BaseFragment;
+import com.funny.developers.musicstylelist.definition.Define;
+import com.funny.developers.musicstylelist.parameterurl.ParameterUrl;
+import com.funny.developers.musicstylelist.parser.SearchListParser;
+import com.funny.developers.musicstylelist.parser.SearchPlayListParser;
+import com.funny.developers.musicstylelist.requesturl.RequestUrl;
+import com.funny.developers.musicstylelist.view.SearchPlayListView;
+
+public class FragmentForPlayList extends BaseFragment{
+
+	private String requestQuery = null;
+	private int requestType = 0;
+	
+	SearchPlayListView playListView = null;
+	PlayListAdapter playListAdapter = null;
+	SearchPlayListParser listParser = null;
+	
+	TextView emptyListView = null;
+	
+	@Override
+	public void settingView() {
+		emptyListView = (TextView)getView().findViewById(R.id.empty_list_view);
+		
+		playListView = (SearchPlayListView) getView().findViewById(R.id.play_list_view);
+		playListView.setEmptyView(emptyListView);
+		playListAdapter = new PlayListAdapter(getActivity());
+		listParser = new SearchPlayListParser();
+	}
+
+	@Override
+	public int registerContentView() {
+		return R.layout.fragment_for_play_list;
+	}
+
+	@Override
+	public void setSearchQueryType(String query, int type) {
+		requestQuery = query;
+		requestType = type;
+		
+		onRequestStart(getActivity(), this);
+	}
+
+	@Override
+	public int getRequestType() {
+		return BaseConsts.REQUEST_TYPE_GET;
+	}
+
+	@Override
+	public int getResultType() {
+		return BaseConsts.RESULT_TYPE_JSON;
+	}
+
+	@Override
+	public String onRequestGetParameter() {
+		
+		if(requestType == Define.TOTAL_SEARCH){
+			return ParameterUrl.SearchTotalTrack(requestQuery);
+			
+		} else if(requestType == Define.YOUTUBE_SEARCH){
+			return ParameterUrl.SearchYoutubeTrack(requestQuery, 1);
+			
+		} else if(requestType == Define.SOUND_CLOUD_SEARCH){
+			return ParameterUrl.SearchSoundCloudTrack(requestQuery, 0);
+		}
+		
+		return null;
+	}
+
+	@Override
+	public String onRequestGetUrl() {
+		
+		if(requestType == Define.TOTAL_SEARCH){
+			return null;
+			
+		} else if(requestType == Define.YOUTUBE_SEARCH){
+			return RequestUrl.SearchYoutubePlayListUrl();
+			
+		} else if(requestType == Define.SOUND_CLOUD_SEARCH){
+			return RequestUrl.SearchSoundCloudPlayListUrl();
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void onRequestResult(Object result, int resultType) {
+		
+		if(resultType == BaseConsts.RESULT_TYPE_JSON){
+			listParser.setJsonObject((JSONObject)result);
+			listParser.parseJsonData();
+			
+			if(!listParser.getList().isEmpty()){
+				playListAdapter.setList(listParser.getList()); // µ•¿Ã≈Õ ª¿‘
+				playListView.setAdapter(playListAdapter);
+			}
+		} else {
+			
+		}
+		
+	}
+}
