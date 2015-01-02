a84321ca5796dbd4a9ab6e8eefb4de376ae5080a
kim327 <k3s2t7@nate.com>
2015-01-02 18:57:56 +0900

    APP ver1.0
    1. add UserPlayList (sqlite)

---
+++ Mobile/APP/MusicStyleList 1.0ver/MusicStyleList/src/com/funny/developers/musicstylelist/baseactivity/BaseActivity.java
@@ -0,0 +1,69 @@
+package com.funny.developers.musicstylelist.baseactivity;
+
+import com.funny.developers.baseconnectionlib.asnycktask.BaseAsyncTask;
+import com.funny.developers.baseconnectionlib.asnycktask.BaseAsyncTask.OnRequestInterFace;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+abstract public class BaseActivity extends Activity implements OnRequestInterFace{
+	
+	abstract protected void settingView();
+	abstract protected int registerContentView();
+	
+	protected BaseAsyncTask connectVender;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(registerContentView());
+		settingView();
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+	
+	@Override
+	public void onStop() {
+		super.onStop();
+	}
+	
+	@Override
+	public void onRestart() {
+		super.onRestart();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+	
+	public void onRequestStart(Context context, OnRequestInterFace callback){
+		connectVender = new BaseAsyncTask(context, callback);
+		connectVender.execute();
+	}
+	
+	@Override
+	public void onRequestError(int errorType) {
+		//process error 
+		onRequestCancle();
+	}
+	
+	@Override
+	public void onRequestCancle() {
+		connectVender.cancel(true);
+	}
+}
