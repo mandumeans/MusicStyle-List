1482d02b96b1a2d0dabc40cedd3bfc8cde143dc9
busungg <busungg@naver.com>
2015-01-04 21:24:35 +0900

    APP, API Web Commit
    1. App 1.2 ver commit
    2. API Web 0.4 ver commit

---
+++ Mobile/APP/MusicStyleList 1.2ver/MusicStyleList/src/com/funny/developers/musicstylelist/model/SearchTrackListModel.java
@@ -0,0 +1,11 @@
+package com.funny.developers.musicstylelist.model;
+
+public class SearchTrackListModel implements BaseModel{
+	
+	public String id = null;
+	public String title = null;
+	public String thumbnail = null;
+	public String uploaded = null;
+	public int viewCount = 0;
+	public int trackType = 0;
+}
