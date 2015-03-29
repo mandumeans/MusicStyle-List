d00e072477272db0446bc0fb7536d000ff8bbfae
busungg <busungg@naver.com>
2015-03-29 13:28:36 +0900

    App clean
    1. App clean

--- Mobile/APP/MusicStyleList 0.3ver/MusicStyleList/src/com/funny/developers/musicstylelist/definition/Define.java
+++
@@ -1,6 +0,0 @@
-package com.funny.developers.musicstylelist.definition;
-
-public class Define {
-	
-	public static final String BASE_API_URL = "http://rlaqnqn.dothome.co.kr/Musiclist/";
-}
--- Mobile/APP/MusicStyleList 0.4ver/MusicStyleList/src/com/funny/developers/musicstylelist/definition/Define.java
+++
@@ -1,9 +0,0 @@
-package com.funny.developers.musicstylelist.definition;
-
-public class Define {
-	
-	public static final String BASE_API_URL = "http://rlaqnqn.dothome.co.kr/Musiclist/";
-	
-	public static final String SEARCH_YOUTUBE_API_URL = "YoutubeKeywordSearch";
-	public static final String SEARCH_SOUNDCLOUD_API_URL = "SoundcloudKeywordSearch";
-}
